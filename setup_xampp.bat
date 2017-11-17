de12d8876cc40616eada645972485aafc3f7c02e
speadup <speadup@gmail.com>
2017-11-17 13:33:47 +0800

    drop some binary files

--- setup_xampp.bat
+++ setup_xampp.bat
@@ -35,7 +35,7 @@ GOTO END
 :Normal
 set PHP_BIN=php\php.exe
 set CONFIG_PHP=install\install.php
-%PHP_BIN% -n -d output_buffering=0 -q %CONFIG_PHP% usb
+%PHP_BIN% -n -d output_buffering=0 %CONFIG_PHP%
 GOTO END
 
 :END
