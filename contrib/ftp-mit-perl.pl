c4cd73719b624cadef0a135693fa2ff3ac5c041d
speadup <speadup@gmail.com>
2017-11-19 11:53:33 +0800

    drop unused and binary files

--- contrib/ftp-mit-perl.pl
+++
@@ -1,7 +0,0 @@
-use Net::FTP;
-print "Content-Type: text/html\n\n";
-$ftp = Net::FTP->new("localhost");	# Unser Host
-die "Konnte keine Verbindung aufbauen $!" unless $ftp;
-$ftp->login("newuser", "wampp");	# Hier Benutzername und Password eingeben			
-$ftp->get("index.php");
-$ftp->quit;
--- contrib/sql2xml.php
+++
@@ -1,9 +0,0 @@
-<?php
-	include_once("XML/sql2xml.php");
-
-	$sql2xml = new xml_sql2xml("mysql://root@localhost/cdcol");
-	$xml = $sql2xml->getxml("select * from cds");
-	
-	header("Content-Type: text/xml;");
-	echo $xml;
-?>
